67c1fe8e5b7a955b69e0e6f13f94cd4a86f05b3b
Aleksandr Ge <aleksandrg@Aleksandrs-MacBook-Pro.local>
2026-09-09 15:23:16 +0300

    Fallback to default Chromium in CI, use system Chrome only locally

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
     launchOptions: slowMo ? { slowMo } : undefined,
   },
   projects: [
-    { name: 'chromium', use: { ...devices['Desktop Chrome'], channel: 'chrome' } },
+    { name: 'chromium', use: { ...devices['Desktop Chrome'], channel: process.env.CI ? undefined : 'chrome' } },
     { name: 'firefox', use: { ...devices['Desktop Firefox'] } },
     { name: 'webkit', use: { ...devices['Desktop Safari'] } },
   ],
